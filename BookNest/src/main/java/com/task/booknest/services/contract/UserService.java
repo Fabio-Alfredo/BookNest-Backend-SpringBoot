66410f2f250fc669da71beeead00e2f6699d4084
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-01 17:32:55 -0600

    add: new contrat for user services :sparkles:

---
+++ BookNest/src/main/java/com/task/booknest/services/contract/UserService.java
@@ -0,0 +1,14 @@
+package com.task.booknest.services.contract;
+
+
+import com.task.booknest.domains.models.Token;
+import com.task.booknest.domains.models.User;
+
+public interface UserService {
+    User findByEmail(String email);
+
+
+    Token registerToken(User user) throws Exception;
+    Boolean isTokenValid(User user, String token) ;
+    void cleanToken(User user) throws Exception;
+}
