d46a63b907b4fd9c10d15674d87e60a9ca7356f4
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-02 11:13:48 -0600

    add: new dto for create new genre :sparkles:

--- BookNest/src/main/java/com/task/booknest/domains/dtos/Genre/CreateGenreDto.java
+++ BookNest/src/main/java/com/task/booknest/domains/dtos/Genre/CreateGenreDto.java
@@ -1,2 +1,12 @@
-package com.task.booknest.domains.dtos.Genre;public class CreateGenreDto {
+package com.task.booknest.domains.dtos.Genre;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreateGenreDto {
+    @NotBlank(message = "id is required")
+    private String id;
+    @NotBlank(message = "name for genre is required")
+    private String genre;
 }
