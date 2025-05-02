2fcb05be8f42612d2ee2b4f01801ff952f65c06f
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-01 18:37:08 -0600

    add: new method for auth users :sparkles:

--- BookNest/src/main/java/com/task/booknest/services/impl/UserServiceImpl.java
+++ BookNest/src/main/java/com/task/booknest/services/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.task.booknest.services.impl;
 
+import com.task.booknest.domains.dtos.auth.LoginDto;
+import com.task.booknest.domains.dtos.auth.RegisterUserDto;
 import com.task.booknest.domains.models.Token;
 import com.task.booknest.domains.models.User;
 import com.task.booknest.respositories.TokenRepository;
 import com.task.booknest.respositories.UserRepository;
 import com.task.booknest.services.contract.UserService;
 import com.task.booknest.utils.JWTTools;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +20,39 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
     private final JWTTools jwtTools;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, TokenRepository tokenRepository, JWTTools jwtTools) {
+    public UserServiceImpl(UserRepository userRepository, TokenRepository tokenRepository, JWTTools jwtTools, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.tokenRepository = tokenRepository;
         this.jwtTools = jwtTools;
+        this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void registerUser(RegisterUserDto user) {
+        try{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+            User existUser = userRepository.findByEmail(user.getEmail());
+            if(existUser!=null){
+                throw  new RuntimeException("User already exists");
+            }
+
+            User newUser = modelMapper.map(user, User.class);
+            newUser.setRoles(List.of(roleService.findById("USER")));
+            userRepository.save(newUser);
+
+        }catch (Exception e){
+            throw  new RuntimeException("Error while create user: "+ e.getMessage());
+        }
+    }
+
+    @Override
+    public Token loginUser(LoginDto auth) {
+        return null;
     }
 
     @Override
@@ -37,6 +69,7 @@ public class UserServiceImpl implements UserService {
         }
     }
 
+    //Method for auth user
     @Override
     public Token registerToken(User user) throws Exception {
         cleanToken(user);
