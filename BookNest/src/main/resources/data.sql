a4332f9254ba22dbf7a497e7a2810dfdcbe026c6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-02 17:16:16 -0600

    add: new configurations and new data for always database :sparkles:

--- BookNest/src/main/resources/data.sql
+++ BookNest/src/main/resources/data.sql
@@ -3,3 +3,10 @@ INSERT INTO public.roles(id, value, description)VALUES ('ADMIN', 'administrador'
 
 INSERT INTO public.roles(id, value, description)VALUES ('USER', 'usuario', 'usuario del sistema')
     ON CONFLICT (id) DO UPDATE SET value = excluded.value, description = excluded.description;
+
+
+INSERT INTO public.genres(id, genre)VALUES('NH', 'Novela historica')
+      ON CONFLICT(id) DO UPDATE SET genre = excluded.genre;
+
+INSERT INTO public.genres(id, genre)VALUES('ROM', 'Romantica')
+    ON CONFLICT(id) DO UPDATE SET genre = excluded.genre;
