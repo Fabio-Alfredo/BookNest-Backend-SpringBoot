b421cda23c4b788ccd9eb3c9cb01cc9129b5352c
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-02 17:02:15 -0600

    add: new dto for create new authro :sparkles:

--- BookNest/src/main/java/com/task/booknest/domains/dtos/Author/CreateAuthorDto.java
+++ BookNest/src/main/java/com/task/booknest/domains/dtos/Author/CreateAuthorDto.java
@@ -1,2 +1,20 @@
-package com.task.booknest.domains.dtos.Author;public class CreateAuthorDto {
+package com.task.booknest.domains.dtos.Author;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Data
+public class CreateAuthorDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "biography is required")
+    private String biography;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+    private MultipartFile file;
 }
