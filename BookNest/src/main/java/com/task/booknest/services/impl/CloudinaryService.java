6dfcb18a5c7d8ec2f76087acdd98881ff46c7405
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-02 10:28:08 -0600

    add: cloudinary configuration and service for upload images :sparkles:

--- BookNest/src/main/java/com/task/booknest/services/impl/CloudinaryService.java
+++ BookNest/src/main/java/com/task/booknest/services/impl/CloudinaryService.java
@@ -1,2 +1,34 @@
-package com.task.booknest.services.impl;public class CloudinaryService {
+package com.task.booknest.services.impl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImage(MultipartFile imagePath, String folderName) {
+        try {
+            Map<Object, Object> options = new HashMap<>();
+            options.put("folder", folderName);
+            Map uploadFile = cloudinary.uploader().upload(imagePath.getBytes(), options);
+            String publicId = (String) uploadFile.get("public_id");
+            return cloudinary.url().secure(true).generate(publicId);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException("Error uploading image to Cloudinary", e);
+        }
+    }
 }
