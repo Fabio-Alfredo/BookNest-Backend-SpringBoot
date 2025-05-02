adf5121160e50681fa5309b4393b7bfb6224149a
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-01 18:42:11 -0600

    add: implementation service for find roles :sparkles:

--- BookNest/src/main/java/com/task/booknest/services/impl/RoleServiceImpl.java
+++ BookNest/src/main/java/com/task/booknest/services/impl/RoleServiceImpl.java
@@ -1,2 +1,29 @@
-package com.task.booknest.services.impl;public class RoleServiceImpl {
+package com.task.booknest.services.impl;
+
+import com.task.booknest.domains.models.Role;
+import com.task.booknest.respositories.RoleRepository;
+import com.task.booknest.services.contract.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role findById(String id) {
+        try{
+            Role role = roleRepository.findById(id).orElse(null);
+            if(role ==null){
+                throw new RuntimeException("Role not exists");
+            }
+            return role;
+        }catch (Exception e){
+            throw  new RuntimeException("Error while find role: " +e.getMessage());
+        }
+    }
 }
